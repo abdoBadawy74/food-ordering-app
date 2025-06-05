5a4b991a531ac7499f0b0a22675fc7ffc3a3a4cf
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2025-06-05 18:34:35 +0300

    feat: remove unused database creation logic and add last stopped time to info.txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,7 @@
-import { db } from "@/lib/prisma";
 import BestSeller from "./_components/BestSeller";
 import Hero from "./_components/Hero";
 
 export default async function Home() {
-  const size = await db.extra.createMany({
-    data: [
-      {
-        name: "BASIL",
-        price: 1.5,
-        productId: "fdgfhgfgfj",
-      },
-      {
-        name: "CHEESE",
-        price: 3,
-        productId: "fdgfhgfgfj",
-      },
-      {
-        name: "ONION",
-        price: 3.2,
-        productId: "fdgfhgfgfj",
-      },
-      
-    ],
-  });
   return (
     <main>
       <Hero />
