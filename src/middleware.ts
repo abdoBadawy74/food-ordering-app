51357668e00e7ec42f949fc7bc10c11de8198c6d
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2025-07-31 20:38:12 +0300

    feat: add i18n configuration and middleware for locale handling; update dependencies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,47 @@
-// import { match } from '@formatjs/intl-localematcher'
-// import Negotiator from 'negotiator'
-
 import { NextRequest, NextResponse } from "next/server";
-
-// let headers = { 'accept-language': 'en-US,en;q=0.5' }
-// let languages = new Negotiator({ headers }).languages()
-// let locales = ['en-US', 'nl-NL', 'nl']
-// let defaultLocale = 'en-US'
-
-// match(languages, locales, defaultLocale) // -> 'en-US'
+import { match as matchLocale } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { i18n, LanguageType } from "./i18n.config";
 
 export async function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
 
   requestHeaders.set("x-url", request.url);
 
+  const pathname = request.nextUrl.pathname;
+
+  const pathnameIsMissingLocale = i18n.locales.every(
+    (locale) => !pathname.startsWith(`/${locale}/`)
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
+  }
   return NextResponse.next({
     request: {
       headers: requestHeaders,
     },
   });
+}
+
+// Redirect if there is no locale
+function getLocale(request: NextRequest): string | undefined {
+  const negotiatorHeaders: Record<string, string> = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+  const locales: LanguageType[] = i18n.locales;
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+  let locale = "";
+
+  try {
+    locale = matchLocale(languages, locales, i18n.defaultLocale);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  } catch (error: any) {
+    locale = i18n.defaultLocale;
+  }
+  return locale;
 }
 
 export const config = {
