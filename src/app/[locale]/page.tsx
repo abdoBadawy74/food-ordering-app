176fdff9102c3ad5bbea7a23fc47758774ead9a2
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2025-08-01 12:48:34 +0300

    feat: implement localization support with language dictionaries and translation function; update layout for dynamic language handling
    اخر حاجة عشان ماشي الجيش ، تحيا مصر

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,8 +2,17 @@ import About from "@/components/about";
 import BestSeller from "./_components/BestSeller";
 import Hero from "./_components/Hero";
 import Contact from "@/components/contact";
+import getTrans from "@/lib/translation";
+import { headers } from "next/headers";
+import { Locale } from "@/i18n.config";
 
 export default async function Home() {
+  const url = (await headers()).get("x-url");
+
+  const locale = url?.split("/")[3] as Locale;
+
+  const { } = await getTrans(locale);
+
   return (
     <main>
       <Hero />
